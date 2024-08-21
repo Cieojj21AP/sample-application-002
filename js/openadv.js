12877479d9b5fbe34fd381ff68ace063d4fc5a5c
user <user@example.com>
2024-08-21 19:47:11 +0900

    add openadv.js

---
+++ js/openadv.js
@@ -0,0 +1,7 @@
+// 広告タブを開く
+function tabopenAdv() {
+    document.getElementById('adv-ninja').click();
+}
+
+// リンククリックで関数を実行
+document.getElementById("openXLandau").addEventListener("click", tabopenAdv);
