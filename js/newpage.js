5af27693c2a3c01c2867538b8962fc53abb3038b
takenoko92 <dhefoekknn221@gmail.com>
2026-02-15 22:08:48 +0900

    fix url

--- js/newpage.js
+++ js/newpage.js
@@ -3,93 +3,96 @@ function tabopenXLandau() {
     window.open("https://twitter.com/landau5526", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openXLandau").addEventListener("click", tabopenXLandau);
 
 // 新しいタブでXUlandauを開く
 function tabopenXUlandau() {
     window.open("https://twitter.com/u_adnal", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openXUlandau").addEventListener("click", tabopenXUlandau);
 
 // 新しいタブでXUlandauを開く
 function tabopenPixiv() {
     window.open("https://www.pixiv.net/users/57868217", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openPixiv").addEventListener("click", tabopenPixiv);
 
 // 新しいタブでXUlandauを開く
 function tabopenBlueSky() {
     window.open("https://bsky.app/profile/landau5.bsky.social", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openBlueSky").addEventListener("click", tabopenBlueSky);
 
 // 新しいタブでXUlandauを開く
 function tabopenFantia() {
     window.open("https://fantia.jp/fanclubs/192395", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openFantia").addEventListener("click", tabopenFantia);
 
 // 新しいタブでXUlandauを開く
 function tabopenYoutube() {
     window.open("https://www.youtube.com/@landau5526", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openYoutube").addEventListener("click", tabopenYoutube);
 
 // 新しいタブでXUlandauを開く
 function tabopenDeviantart() {
     window.open("https://www.deviantart.com/landau5526", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openDeviantart").addEventListener("click", tabopenDeviantart);
 
 // 新しいタブでXUlandauを開く
 function tabopenSizedon() {
     window.open("https://misskey.sizedon.com/@landau", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openSizedon").addEventListener("click", tabopenSizedon);
 
 // 新しいタブでXUlandauを開く
 function tabopenMissky() {
     window.open("https://misskey.io/@landau1", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openMissky").addEventListener("click", tabopenMissky);
 
 // 新しいタブでXUlandauを開く
 function tabopenOdaibako() {
     window.open("https://odaibako.net/u/landau5526", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openOdaibako").addEventListener("click", tabopenOdaibako);
 
 // 新しいタブでFanboxを開く
 function tabopenFanbox() {
     window.open("https://landau.fanbox.cc/", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openFanbox").addEventListener("click", tabopenFanbox);
 
 // 新しいタブでPatreonを開く
 function tabopenPatreon() {
     window.open("https://www.patreon.com/landau/", "_blank");
 }
 
-// リンククリックで関数を実行
-document.getElementById("openPatreon").addEventListener("click", tabopenPatreon);
+// イベントリスナーを設定する
+function setEventOpen(){
+    // リンククリックで関数を実行
+    document.getElementById("openXLandau").addEventListener("click", tabopenXLandau);
+    // リンククリックで関数を実行
+    document.getElementById("openXUlandau").addEventListener("click", tabopenXUlandau);
+    // リンククリックで関数を実行
+    document.getElementById("openPixiv").addEventListener("click", tabopenPixiv);
+    // リンククリックで関数を実行
+    document.getElementById("openBlueSky").addEventListener("click", tabopenBlueSky);
+    // リンククリックで関数を実行
+    document.getElementById("openFantia").addEventListener("click", tabopenFantia);
+    // リンククリックで関数を実行
+    document.getElementById("openYoutube").addEventListener("click", tabopenYoutube);
+    // リンククリックで関数を実行
+    document.getElementById("openDeviantart").addEventListener("click", tabopenDeviantart);
+    // リンククリックで関数を実行
+    document.getElementById("openSizedon").addEventListener("click", tabopenSizedon);
+    // リンククリックで関数を実行
+    document.getElementById("openMissky").addEventListener("click", tabopenMissky);
+    // リンククリックで関数を実行
+    document.getElementById("openOdaibako").addEventListener("click", tabopenOdaibako);
+    // リンククリックで関数を実行
+    document.getElementById("openFanbox").addEventListener("click", tabopenFanbox);
+    // リンククリックで関数を実行
+    document.getElementById("openPatreon").addEventListener("click", tabopenPatreon);
+}
